5ba6e872bd2b4fbb2a697b025977f7b1a21d0302
Nikolay Potapov <potapov4444gmail.ru>
2021-11-22 01:51:12 +0700

    first commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,101 @@
+package ru.netology.domain;
+
+public class Radio {
+
+    private int minStation;
+    private int maxStation;
+    private int currentStation;
+
+    private int minVolume;
+    private int maxVolume;
+    private int currentVolume;
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void increaseStation() {
+        if (currentStation < 10) {
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void turnDownTheStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        }
+    }
+
+///////////////////Volume/////////////////
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+    public void decreaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+
+}
+
+
+
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,64 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void indicateTheStationNumber() {
+        Radio radio = new Radio();
+        radio.setMaxStation(10);
+        radio.setMinStation(0);
+        radio.setCurrentStation(10);
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    public void stationIncrease() {
+        Radio radio = new Radio();
+        radio.setMaxStation(10);
+        radio.setMinStation(0);
+        radio.increaseStation();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    public void stationDecrease() {
+        Radio radio = new Radio();
+        radio.setMaxStation(10);
+        radio.setMinStation(0);
+        radio.turnDownTheStation();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+//////////////////Volume//////////////////////
+    @Test
+    public void setVolumeLevel() {
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.setCurrentVolume(1);
+        assertEquals(1, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void increaseInSoundLevel() {
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.increaseVolume();
+        assertEquals(1, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void decreaseInSoundLevel() {
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.decreaseVolume();
+        assertEquals(1, radio.getCurrentVolume());
+    }
+
+}
